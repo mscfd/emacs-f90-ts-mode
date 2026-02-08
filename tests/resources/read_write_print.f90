ae175e395af79b1281097a2fbe1e51b38667a708
Martin Stein <mscfd@gmx.net>
2026-02-08 22:54:12 +0100

    Handle keywords as label names in block structures
    The AST subtly changes its structure if a label name is reserved keyword, like:
    
         write: if (result > 10) then
              write(unit,'(es15.5,f12.3)') expr, result
         end if write
    
    But maybe, this should better be solved at the grammar level?

--- tests/resources/read_write_print.f90
+++ tests/resources/read_write_print.f90
@@ -1,15 +1,15 @@
 subroutine print()
 
      read: associate(x => arr(2))
-     read(*,*) x
+          read(*,*) x
      end associate read
-     
+
      print: do value = 1,10
-     print *, 'hello', value
+          print *, 'hello', value
      end do print
 
      write: if (result > 10) then
-     write(unit,'(es15.5,f12.3)') expr, result
+          write(unit,'(es15.5,f12.3)') expr, result
      end if write
 
 end subroutine print
