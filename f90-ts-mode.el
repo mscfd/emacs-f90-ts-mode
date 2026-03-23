542657067f351e84f68c6d450bcdb8aaeb7a459d
Martin Stein <mscfd@gmx.net>
2026-03-23 21:56:15 +0100

    Add copyright to el-headers

--- f90-ts-mode.el
+++ f90-ts-mode.el
@@ -9,8 +9,22 @@
 ;; Version: 0.1.2pre
 ;; Package-Requires: ((emacs "30.1"))
 
-;; Provides syntax highlighting and structural navigation for Fortran 90+
-;; files using the Tree-sitter parsing engine.
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
